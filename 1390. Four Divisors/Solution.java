1a82783dfb3bda75bd12da20365e3095e4f7e8d0
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2026-01-04 19:29:59 +0000

    Implement sumFourDivisors method in Solution class

---
+++ 1390. Four Divisors/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        int totalSum = 0;
+
+        for (int num : nums) {
+            int count = 0;
+            int currentSum = 0;
+
+            for (int i = ; i*i<=num; i++) {
+                if (num % i == 0) {
+                    if (i*i == num) {
+                        count++;
+                        currentSum += i;
+                    } else {
+                        count += 2;
+                        currentSum += i + (num/i);
+                    }
+                }
+                if (count > 4) break;
+            }
+            if (count == 4 ){
+                totalSum += currentSum;
+            }
+        }
+        return totalSum;
+    }
+}
