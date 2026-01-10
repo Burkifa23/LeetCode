1f3f1c458f2282aa6602303f6d6e697d3fcbb912
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2026-01-10 22:00:53 +0000

    Implement finalPrices method using monotonic stack

---
+++ DSA Quest/Monotonic Stack/Q1. Final Prices With a Special Discount in a Shop/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] answer = prices.clone();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i=0; i<n; i++){
+            while(!stack.isEmpty() && prices[i] <= prices[stack.peek()]){
+                int indexToDiscount = stack.pop();
+                answer[indexToDiscount] = prices[indexToDiscount] - prices[i];
+            }
+            stack.push(i);
+        }
+        return answer;
+    }
+}
