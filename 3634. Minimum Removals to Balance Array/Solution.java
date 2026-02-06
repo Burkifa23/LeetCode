344b06e7bb64d9462d9e9d291175d2cab4bc6f9a
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2026-02-06 23:17:09 +0000

    Implement minRemoval method to balance array

---
+++ 3634. Minimum Removals to Balance Array/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+class Solution {
+    public int minRemoval(int[] nums, int k) {
+        Arrays.sort(nums);
+        int left= 0;
+        int maxLen=0;
+        int n=nums.length;
+
+        for( int right=0;right<n;right++){
+            while((long) nums[right]> (long)nums[left]*k ){
+                left++;
+            }
+            maxLen= Math.max(maxLen, right-left+1);
+        }
+        return n-maxLen;
+    }
+}
