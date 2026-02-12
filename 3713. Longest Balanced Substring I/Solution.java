6d2775b828b7d3f4c8514f0b3d3d074a6915c394
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2026-02-12 22:48:02 +0000

    Add longestBalanced method in Solution class
    Implement method to find the longest balanced substring.

---
+++ 3713. Longest Balanced Substring I/Solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int longestBalanced(String s) {
+        int n = s.length();
+        int maxLen=0;
+
+        for(int i=0; i<n; i++){
+            int[] freq= new int[26];
+            int distinct = 0;
+            int maxFreq = 0;
+
+            for (int j=i;j<n;j++){
+                int charIdx = s.charAt(j)-'a';
+                
+                if (freq[charIdx]==0){
+                    distinct++;
+                }
+
+                freq[charIdx]++;
+            
+
+                maxFreq = Math.max(maxFreq, freq[charIdx]);
+
+                int currentLen = j-i+1;
+
+                if(maxFreq*distinct==currentLen){
+                    maxLen = Math.max(maxLen, currentLen);
+                }
+
+
+        }
+    }
+    return maxLen;
+}
+}
