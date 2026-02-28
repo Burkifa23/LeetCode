4c31bb5a587e5cc8fe21546c536ddc5fa4dfc023
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2026-02-28 18:59:44 +0000

    Implement concatenatedBinary method in Solution class

---
+++ 1680. Concatenation of Consecutive Binary Numbers/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int concatenatedBinary(int n) {
+        long result =0;
+        long mod = 1_000_000_007;
+        int bitLength = 0;
+
+        for(int i=1; i<=n; i++){
+            if((i&(i-1))==0){
+                bitLength++;
+            }
+            result=((result<<bitLength)+i)%mod;
+        }
+
+        return (int) result;
+    }
+}
