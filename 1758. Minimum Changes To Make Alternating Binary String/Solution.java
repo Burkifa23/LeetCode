e593e27aa4bcbed9c22b392f6e631da390220ea9
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2026-03-05 20:41:56 +0000

    1758. Minimum Changes To Make Alternating Binary String

---
+++ 1758. Minimum Changes To Make Alternating Binary String/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minOperations(String s) {
+        int count = 0;
+        int n= s.length();
+
+        for (int i=0;i<n;i++){
+            if(s.charAt(i)-'0' != i%2){
+                count++;
+            }
+        }
+        
+        return Math.min(count, n-count);
+    }
+}
