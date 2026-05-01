a67d05945cd66a2b43fa615f5d3cf77bbdd60987
FRANK KWIZERA MUGWANEZA <97257798+Burkifa23@users.noreply.github.com>
2026-05-01 14:30:00 +0000

    396. Rotate Functions

---
+++ 396. Rotate Functions/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        int currentF = 0;
+
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            currentF += i * nums[i];
+        }
+
+        int maxF = currentF;
+
+
+        for (int k = 1; k < n; k++) {
+            currentF = currentF + sum - n * nums[n - k];
+            maxF = Math.max(maxF, currentF);
+        }
+
+        return maxF;
+    }
+}
